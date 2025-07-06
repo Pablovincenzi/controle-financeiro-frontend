60ae9248baa1c8b29abc222510c17da349554dd6
Pablovincenzi <pablovincenzi@sysmo.com.br>
2025-07-06 16:14:08 -0300

    Projeto Angular inicial

---
+++ apps/shell/webpack.config.js
@@ -0,0 +1,11 @@
+const {
+  shareAll,
+  withModuleFederationPlugin
+} = require('@angular-architects/module-federation/webpack');
+
+module.exports = withModuleFederationPlugin({
+  name: 'login',
+  exposes: { './Module': 'apps/login/src/app/module/login.module.ts' },
+  shared: shareAll({ singleton: true, strictVersion: true, requiredVersion: 'auto' }),
+  sharedMappings: ['@sysmo-cloud/shared/data-access']
+});
